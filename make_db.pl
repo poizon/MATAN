6731b5e488cdc60b7ca1b7f2485948e157b12393
Pavel <pkuptsov@gmail.com>
2015-10-13 20:52:50 +0300

    add

--- make_db.pl
+++ make_db.pl
@@ -5,10 +5,10 @@ use Rose::DB::Object::Loader;
 
 my $loader = 
     Rose::DB::Object::Loader->new(
-      db_dsn       => 'dbi:mysql:dbname=web;host=qiwi.giftec.ru',
+      db_dsn       => 'dbi:mysql:dbname=web;host=qiwi.server.ru',
       db_username  => 'web',
       db_password  => 'web',
       db_options   => { AutoCommit => 1, ChopBlanks => 1, mysql_enable_utf8 => 1 },
       class_prefix => 'MTN');
     
-  $loader->make_modules(module_dir => 'd:\Dropbox\GIT\MATAN\MTN2');
+  $loader->make_modules(module_dir => '/MTN');
